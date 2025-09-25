610409843f6cfee00ec2b1fcbcab288f24bd9242
MarcosPauloG7 <paulo.marcos1@escolar.ifrn.edu.br>
2025-09-25 16:48:23 -0300

    Reparação da página de login

--- Login.js
+++ Login.js
@@ -26,3 +26,37 @@ function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+function aplicarTema(tema) {
+    const body = document.body;
+    const iconeTema = document.getElementById("icone-tema");
+    const logo = document.getElementById("logo");
+
+
+    if (tema === "dark") {
+        body.classList.add("dark-mode");
+        body.classList.remove("light-mode");
+        if (iconeTema) {
+            iconeTema.textContent = "wb_sunny"
+        }
+        if (logo) {
+            logo.src = "img/LOGO-DARK.png"
+            logo.style.width = "64px";
+            logo.style.heith = "64px";
+        }
+    } else {
+        body.classList.add("light-mode");
+        body.classList.remove("dark-mode");
+        if (iconeTema) {
+            iconeTema.textContent = "bedtime";
+        }
+        if (logo) {
+            logo.src = "img/LOGO.png"
+            logo.style.width = "72px";
+            logo.style.heith = "72px";
+        }
+    }
+    localStorage.setItem("theme", tema);
+}
+
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
